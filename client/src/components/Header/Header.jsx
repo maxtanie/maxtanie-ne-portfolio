cd78b0d8e827ac6c03f39b8390039cb538865081
maxtanie <maxtaniepetitdol@outlook.fr>
2020-08-05 01:46:35 +0200

    Added new styles change

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,32 +1,30 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import "./Header.scss";
-import Nav from "../Nav/Nav";
-import Button from "../Button/Button";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Header.scss';
+import Nav from '../Nav/Nav';
+import Button from '../Button/Button';
 
 export default class Header extends Component {
-  render() {
-    return (
-      <header id="bg-header">
-        <div className="container-700-px center absolute-center">
-          <h1 className="title orange orange-triangle">Hello,</h1>
-          <p className="txt-header">
-            Je m'appelle <b>Maxtanie Petit Dol, </b> j'ai 24 ans, je suis un
-            <b> Webdesigner</b>
-            <br />
-            et <b>une développeuse front end junior</b> située à Paris.
-          </p>
-          <Link className="decoration-none center">
-            <Button
-              width="190px"
-              background="#FC3000"
-              color="#fff"
-              btnName="My Resume"
-              mTop="55"
-            />
-          </Link>
-        </div>
-      </header>
-    );
-  }
+	render() {
+		return (
+			<header id="bg-header">
+				<div className="container-700-px center absolute-center">
+					<h1 className="title orange orange-triangle">Hello,</h1>
+					<p className="txt-header regular">
+						Je m'appelle Maxtanie Petit Dol, j'ai 24 ans, je suis un Webdesigner et une développeuse front
+						end junior située à Paris.
+					</p>
+					<Link className="decoration-none center">
+						<Button
+							width="200px"
+							background="#FC3000"
+							color="#fff"
+							btnName="Télécharger mon CV"
+							mTop="30"
+						/>
+					</Link>
+				</div>
+			</header>
+		);
+	}
 }
